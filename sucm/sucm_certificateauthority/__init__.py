d182b1c9fc05c69ee7e056e85af441960b926e89
rath6587 <rath6587@rath6587.it.su.se>
2023-12-14 17:41:52 +0100

    added some comments

--- sucm/sucm_certificateauthority/__init__.py
+++ sucm/sucm_certificateauthority/__init__.py
@@ -4,7 +4,13 @@ from abc import ABC, abstractmethod
 class SucmCertificateAuthority(ABC):
     @abstractmethod
     def fetch_cert(self, csr_pem):
+        """
+        Returns a cert file in bytecode from the cert provider
+        """
         pass
 
     def revoke_cert(self, fullchain_pem):
+        """
+        Revokes the cert on the cert provider
+        """
         pass
--- sucm/sucm_secret/__init__.py
+++ sucm/sucm_secret/__init__.py
@@ -4,13 +4,25 @@ from abc import ABC, abstractmethod
 class SucmSecret(ABC):
     @abstractmethod
     def get_all_paths(self):
+        """
+        Returns a list of paths
+        """
         pass
 
     def read_secret(self, file_name):
+        """
+        returns file in bytecode
+        """
         pass
 
     def modify_or_create_secret(self, file_name, file_data):
+        """
+        rotates the files in the storage system (vault or whatever)
+        """
         pass
 
     def remove_secret(self, file_name):
+        """
+        removes the secret from the storage system
+        """
         pass
